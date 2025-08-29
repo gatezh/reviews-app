b4960638bd341cbad9bc01ca56ab5306be9fa8fb
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-08-29 17:01:50 +0000

    Add Header component, cleanup

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header(): React.ReactElement {
+  const styles = {
+    padding: '0 1rem',
+    backgroundColor: 'black',
+    color: 'white',
+    lineHeight: '3rem',
+    // textAlign: 'left'
+  }
+
+  return <header style={styles}>⭐️ Latest reviews</header>;
+}
