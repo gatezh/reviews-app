7a3d7cce51decabbb361bd0f502a96499fbc26b7
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-08-29 17:28:15 +0000

    Add Main and Footer dummy components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer(): React.ReactElement {
+  const styles = {};
+
+  return <footer style={styles}>Footer</footer>;
+}
---
+++ src/components/Main.tsx
@@ -0,0 +1,5 @@
+export default function Main(): React.ReactElement {
+  const styles = { };
+
+  return <main style={styles}>Main</main>;
+}
