4bf59e0f4698693f3dfbca9637521d192cdf9810
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-09-11 16:23:30 +0000

    Add basic footer with styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 export default function Footer(): React.ReactElement {
-  const styles = {};
+  const year = new Date().getFullYear();
 
-  return <footer style={styles}>Footer</footer>;
+  return (
+    <footer className="color bg-black p-5 text-white">
+      Copyright {year} All right reserved
+    </footer>
+  );
 }
