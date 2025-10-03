13aec3cfb980f571b7c1ce016176a40048c174e0
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-03 16:43:19 +0000

    Add react router routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
