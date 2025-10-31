5e23999c6e1fdc0fac0cf8922b84fa4061b473e2
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-31 19:35:48 +0000

    Extract ReviewsList into a separate component

---
+++ src/components/ReviewsList.tsx
@@ -0,0 +1,19 @@
+import type React from "react";
+import Review from "./Review";
+import type { ReviewProps } from "../types/review";
+
+type ReviewsListProps = {
+  reviews: ReviewProps[];
+};
+
+const ReviewsList: React.FC<ReviewsListProps> = ({ reviews }) => {
+  return (
+    <div>
+      {reviews.map((review) => (
+        <Review {...review} />
+      ))}
+    </div>
+  );
+};
+
+export default ReviewsList;
