19c02fd76db742946ce3036c5446408767346d2b
Serge Gatezh <2880401+gatezh@users.noreply.github.com>
2025-10-31 22:33:03 +0000

    Use inline styles for Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,9 @@
-// import styles from "./style.css";
-
 const Header = (): React.ReactElement => {
-  const styles = {
-    padding: "0 1rem",
-    backgroundColor: "black",
-    color: "white",
-    lineHeight: "3rem",
-    // textAlign: 'left'
-  };
-
-  return <header style={styles}>⭐️ Latest reviews</header>;
+  return (
+    <header className="bg-black px-4 leading-[3rem] text-white">
+      ⭐️ Latest reviews
+    </header>
+  );
 };
 
 export default Header;
